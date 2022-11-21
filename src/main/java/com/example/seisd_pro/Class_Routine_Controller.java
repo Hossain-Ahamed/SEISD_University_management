fe2d41448efdb86838ee7d73f8c36d07ba1df4ef
Hossain-Ahamed <hossainahamed6872@gmail.com>
2022-11-21 23:37:25 +0600

    intialize add

--- src/main/java/com/example/seisd_pro/Class_Routine_Controller.java
+++ src/main/java/com/example/seisd_pro/Class_Routine_Controller.java
@@ -1,9 +1,21 @@
 package com.example.seisd_pro;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class Class_Routine_Controller{
+    static Connection c1;
+    static Statement s;
 
 
+    @FXML
+    void initialize() {
+        this.c1 = jdbc.c1;
+        this.s = jdbc.s;
+
+    }
 }
--- src/main/java/com/example/seisd_pro/Login_Controller.java
+++ src/main/java/com/example/seisd_pro/Login_Controller.java
@@ -1,14 +1,26 @@
 package com.example.seisd_pro;
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Login_Controller {
+
+    @FXML
+    BorderPane borderpane;
+    static Connection c1;
+    static Statement s;
 
     public void login(ActionEvent event) throws IOException {
 
@@ -18,6 +30,16 @@ public class Login_Controller {
         window.setScene(fxml2scene);
         window.show();
     }
+
+
+    @FXML
+    void initialize(URL url, ResourceBundle rb) throws IOException {
+        this.c1 = jdbc.c1;
+        this.s = jdbc.s;
+
+
+    }
+
 
 
 }
