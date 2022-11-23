501fd8e514b665bdac79f64f2760eac3af60f1b2
tamanna141101 <tamannaislam.20100026@rpsu.edu.bd>
2022-11-24 01:00:39 +0600

    exam routine FXML file added

--- src/main/java/com/example/seisd_pro/Exam_Routine_Controller.java
+++ src/main/java/com/example/seisd_pro/Exam_Routine_Controller.java
@@ -2,20 +2,43 @@ package com.example.seisd_pro;
 
 import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.Statement;
+import java.util.ResourceBundle;
 
 public class Exam_Routine_Controller{
     static Connection c1;
     static Statement s;
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ChoiceBox<String> ECategory;
+
+    @FXML
+    private ChoiceBox<String> dept;
+
+    @FXML
+    private ChoiceBox<String> session;
+
+
 
 
     @FXML
     void initialize() {
         this.c1 = jdbc.c1;
         this.s = jdbc.s;
+        session.getItems().add("Fall 2022");
+        dept.getItems().add("CSE");
+        ECategory.getItems().add("Final");
+
 
     }
 }
