e86da15ddaa14c632dca01fcc40a5d544a9e3eed
tamanna141101 <tamannaislam.20100026@rpsu.edu.bd>
2022-11-25 01:28:31 +0600

    Design Done Group D v1.1

--- src/main/java/com/example/seisd_pro/View_Exam_Routine_Controller.java
+++ src/main/java/com/example/seisd_pro/View_Exam_Routine_Controller.java
@@ -4,12 +4,21 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class View_Exam_Routine_Controller {
 
     static Connection c1;
     static Statement s;
+    public TableView ESchedule_table;
+    public TableColumn Col_Exam_Date;
+    public TableColumn Col_Course_Name;
+    public TableColumn Col_Time;
+    public TableColumn Col_Room;
     @FXML
     private ResourceBundle resources;
 
@@ -24,4 +33,6 @@ public class View_Exam_Routine_Controller {
 
     }
 
+    public void DeleteExamRoutine(ActionEvent actionEvent) {
+    }
 }
