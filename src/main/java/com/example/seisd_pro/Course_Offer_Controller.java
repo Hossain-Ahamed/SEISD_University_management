2560767b0afdc7f09383310ad2c81a76d6b7b610
RaaS <gazitahsin323@gmail.com>
2022-12-12 00:53:14 +0600

    Group A main page created

--- src/main/java/com/example/seisd_pro/Course_Offer_Controller.java
+++ src/main/java/com/example/seisd_pro/Course_Offer_Controller.java
@@ -1,32 +1,77 @@
 package com.example.seisd_pro;
 
-import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
-
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.Statement;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
-public class Course_Offer_Controller{
-
+public class Course_Offer_Controller {
     static Connection c1;
     static Statement s;
 
     @FXML
-    private Button thebutton;
+    private ResourceBundle resources;
 
     @FXML
-    void initialize() {
+    private URL location;
 
-        this.c1 = jdbc.c1;
-        this.s = jdbc.s;
+    @FXML
+    private Label AttendanceNum;
+
+    @FXML
+    private Label MakeUpClassNum;
+
+    @FXML
+    private Label PaymentNum;
+
+    @FXML
+    private Label QuizNum;
+
+    @FXML
+    private Label StudentFailedNum;
+
+    @FXML
+    private Label StudentPassedNum;
+
+    @FXML
+    void Add_Batch(ActionEvent event) {
 
     }
 
+    @FXML
+    void Add_Student(ActionEvent event) {
 
+    }
+
+    @FXML
+    void Assign_Course(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Course_Entry(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Course_View(ActionEvent event) {
+
+    }
+
+    @FXML
+    void New_Sem(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        this.c1 = jdbc.c1;
+        this.s = jdbc.s;
+
+
+    }
 
 }
-
-
-//hdshdsd
