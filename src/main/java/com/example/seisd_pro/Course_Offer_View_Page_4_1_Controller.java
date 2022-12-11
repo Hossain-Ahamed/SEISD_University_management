3979a7a069e77576e04c78ad8e3d6ad04df92ffa
RaaS <gazitahsin323@gmail.com>
2022-12-12 01:00:23 +0600

    Group A File Modified

--- src/main/java/com/example/seisd_pro/Course_Offer_View_Page_4_1_Controller.java
+++ src/main/java/com/example/seisd_pro/Course_Offer_View_Page_4_1_Controller.java
@@ -1,13 +1,21 @@
 package com.example.seisd_pro;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
 public class Course_Offer_View_Page_4_1_Controller {
+
+    static Connection c1;
+    static Statement s;
 
     @FXML
     private ResourceBundle resources;
@@ -16,10 +24,22 @@ public class Course_Offer_View_Page_4_1_Controller {
     private URL location;
 
     @FXML
+    private TableView<Code_Name_Credit_table> Course_table;
+
+    @FXML
     private ChoiceBox<?> batch;
 
     @FXML
     private Button btn_show;
+
+    @FXML
+    private TableColumn<?, ?> col_courseCode;
+
+    @FXML
+    private TableColumn<?, ?> col_courseName;
+
+    @FXML
+    private TableColumn<?, ?> col_credit;
 
     @FXML
     private ChoiceBox<?> semester;
@@ -41,10 +61,12 @@ public class Course_Offer_View_Page_4_1_Controller {
 
     @FXML
     void initialize() {
-        assert batch != null : "fx:id=\"batch\" was not injected: check your FXML file 'Course_Offer_View_Page_4_1.fxml'.";
-        assert btn_show != null : "fx:id=\"btn_show\" was not injected: check your FXML file 'Course_Offer_View_Page_4_1.fxml'.";
-        assert semester != null : "fx:id=\"semester\" was not injected: check your FXML file 'Course_Offer_View_Page_4_1.fxml'.";
+        this.c1 = jdbc.c1;
+        this.s = jdbc.s;
 
+        col_courseCode.setCellValueFactory(new PropertyValueFactory<>("CourseCode"));
+        col_courseName.setCellValueFactory(new PropertyValueFactory<>("CourseName"));
+        col_credit.setCellValueFactory(new PropertyValueFactory<>("CourseCredit"));
     }
 
 }
