fd0b26fa80cb17c0ab97c20a95a86094b5bdf5b6
Hossain-Ahamed <hossainahamed6872@gmail.com>
2022-12-13 21:58:31 +0600

    fresher student regitration page controller to database

--- src/main/java/com/example/seisd_pro/NewStudentEntry.java
+++ src/main/java/com/example/seisd_pro/NewStudentEntry.java
@@ -2,18 +2,21 @@ package com.example.seisd_pro;
 
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 public class NewStudentEntry {
+
     static Connection c1;
     static Statement s;
-
     @FXML
     private ResourceBundle resources;
 
@@ -42,10 +45,84 @@ public class NewStudentEntry {
     private TextField SID;
 
     @FXML
-    private ComboBox<String> semester;
+    private RadioButton female;
 
     @FXML
-    void Save(ActionEvent event) {
+    private RadioButton male;
+
+    @FXML
+    private RadioButton others;
+
+    @FXML
+    private ComboBox<String> semester;
+    static String GenderValue;
+    private static  ToggleGroup group;
+
+    @FXML
+    void Save(ActionEvent event) throws SQLException {
+        String gendervalue=null;
+        RadioButton selected = (RadioButton) group.getSelectedToggle();
+        try {
+            gendervalue = selected.getText();
+        }catch (Exception e){
+            gendervalue ="";
+        }
+
+
+
+        if(utilities.isNotNull(FName.getText().trim())
+        && utilities.isNotNull(LName.getText().trim())
+        && utilities.isNotNull(DOT.getValue().toString())
+        && utilities.isNotNull(Religion.getItems().toString())
+        && utilities.isNotNull(gendervalue)  &&
+                utilities.isNotNull(Address.getText().trim())  &&
+                utilities.isNotNull(SID.getText().trim()) &&
+                utilities.isNotNull(Batch.getText()) &&
+                utilities.isInteger(Batch.getText()) &&
+                utilities.isNotNull(semester.getValue().toString())
+        ){
+
+
+            ResultSet rs = s.executeQuery("SELECT * FROM student where id = "+SID.getText().trim()+"");
+            String id = null;
+            while (rs.next()){
+                id = rs.getString("id");
+            }
+            if(id==null){
+
+
+                JSONObject jsonObj = new JSONObject();
+                JSONArray jsonarray = new JSONArray();
+
+                jsonarray.add(utilities.getStringValueObj("dateOfBirth",DOT.getValue().toString()));
+                jsonarray.add(utilities.getStringValueObj("religion",Religion.getValue().toString()));
+                jsonarray.add(utilities.getStringValueObj("gender", gendervalue));
+                jsonarray.add(utilities.getStringValueObj("address",Address.getText().trim()));
+                jsonObj.put("information",jsonarray);
+                jsonObj.put("name",FName.getText().trim()+" "+LName.getText().trim());
+
+                 s.executeUpdate("INSERT INTO `student` (`id`, `batch`, `admission_sem`, `info`) VALUES ('"+SID.getText().trim()+"', '"+Batch.getText().trim()+"', '"+semester.getValue().toString()+"', '"+JSONValue.toJSONString(jsonObj)+"')");
+
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Confirm");
+                alert.setHeaderText("Successfully added!!!");
+                alert.showAndWait();
+            }else{
+
+               // id taken
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Already Registered ID");
+                alert.showAndWait();
+            }
+        }else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("All data is not provided Correctly");
+            alert.showAndWait();
+        }
+
+
 
     }
 
@@ -54,18 +131,29 @@ public class NewStudentEntry {
 
     }
 
+
     @FXML
     void initialize() {
 
+
         this.c1 = jdbc.c1;
         this.s = jdbc.s;
+        semester.getItems().add(utilities.thisSemester());
         Religion.getItems().add("Islam");
         Religion.getItems().add("Hindu");
         Religion.getItems().add("Christian");
         Religion.getItems().add("Buddhism");
-        Religion.getItems().add("Atheist");
+        Religion.getItems().add("Others");
+        Religion.getItems().add("Not a Believer");
 
-        semester.getItems().add(utilities.thisSemester());
+
+        group= new ToggleGroup();
+        male.setToggleGroup(group);
+        female.setToggleGroup(group);
+        others.setToggleGroup(group);
+
+
+
     }
 
 }
